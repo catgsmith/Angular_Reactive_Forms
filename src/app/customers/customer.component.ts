aefe692904033e42deb18c55ed4ffe31bcabb03e
catgsmith <catgsmith@hotmail.com>
2021-03-29 22:34:57 +0100

    convert to reactive form

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl} from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +10,23 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-  customer = new Customer();
+  customerForm: FormGroup;
+  customer = new Customer(); // data model
 
   constructor() { }
 
   ngOnInit(): void {
+    // form model
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
